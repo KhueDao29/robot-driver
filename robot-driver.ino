aedd502e4238d66bbc8ebf355f54a26590824266
Khue Dao <73230749+KhueDao29@users.noreply.github.com>
2023-09-26 14:22:51 +0700

    Add files via upload

--- robot-driver.ino
+++ robot-driver.ino
@@ -25,7 +25,8 @@ const int K = 30; // adjust K for smooth response
 
 const int speed = 200;
 const int delayTime = 300;
-const int delayTurn = 200;
+const int delayTurn = 180;
+const int delayForward = 200;
 
 void setup()
 {
@@ -287,6 +288,8 @@ String readSensor(int sensorL, int sensorR, int sensorFL, int sensorFR)
   return String(sensorL) + String(sensorR) + String(sensorFL) + String(sensorFR);
 }
 
+
+
 void action(String sensorValue)
 {
   const int value = sensorValue.toInt();
@@ -295,7 +298,7 @@ void action(String sensorValue)
   case 0: // 0000
     Serial.println("Go forward!");
     goForward(speed);
-    delay(delayTime);
+    delay(delayForward);
     break;
 
   case 1: // 0001
@@ -305,27 +308,35 @@ void action(String sensorValue)
     break;
 
   case 10: // 0010
-    Serial.println("Detected wall in front at a left angle. Turn left!");
+    Serial.println("Detected wall in front at a left angle. Go back, then turn right!");
     goBackward(speed);
-    delay(delayTime);
-    turnRight(speed);
+    // delay(delayTime);
+    delay(150);
+    stop();
+    delay(1000);
+    turnRight(speed, 45);
     delay(delayTime);
     break;
 
   case 11: // 0011
     Serial.println("Dectected wall in front with both side wall open. Turn left!");
     goBackward(speed);
-    delay(delayTime);
+    // delay(delayTime);
+    delay(150);
+    stop();
+    delay(1000);
     turnLeft(speed);
     delay(delayTime);
     break;
 
   case 100: // 0100
     Serial.println("Dectected left wall open. Turn left!");
-    turnLeft(speed);
-    delay(delayTime);
-    goForward(speed);
-    delay(delayTime);
+    turnLeft(speed, 50);
+    delay(delayTime*2/3);
+    stop();
+    delay(1000);
+    turnLeft(speed, 50);
+    delay(delayTime*8/9);
     break;
 
   case 101: // 0101
@@ -335,24 +346,24 @@ void action(String sensorValue)
 
   case 110: // 0110
     Serial.println("Detected wall in front at a right angle but too close to right wall. Turn left 135 degrees!");
-    turnLeft(speed, 135);
+    goBackward(speed);
+    delay(100);
+    stop();
+    delay(1000);
+    turnLeft(speed, 90);
     delay(delayTime);
     break;
 
   case 111: // 0111
     Serial.println("Detected wall in front too close and right wall. Go back a bit then turn left!");
     goBackward(speed);
-    delay(300);
-    stop();
-    delay(1000);
-    turnLeft(speed);
-    delay(delayTime);
+    delay(180);
     break;
 
   case 1000:
     Serial.println("Detected left wall close and in front open. Go forward!");
     goForward(speed);
-    delay(delayTime);
+    delay(delayForward);
     break;
 
   case 1001:
@@ -369,7 +380,7 @@ void action(String sensorValue)
   case 1011:
     Serial.println("Detected wall in front and left wall. Go back a bit then turn right!");
     goBackward(speed);
-    delay(300);
+    delay(150);
     stop();
     delay(1000);
     turnRight(speed);
@@ -379,7 +390,7 @@ void action(String sensorValue)
   case 1100:
     Serial.println("Detected both side wall. Go forward!");
     goForward(speed);
-    delay(delayTime);
+    delay(delayForward);
     break;
 
   case 1101:
@@ -401,7 +412,7 @@ void action(String sensorValue)
   default:
     Serial.println("Default. Go forward");
     goForward(speed);
-    delay(delayTime);
+    delay(delayForward);
     break;
   }
 }
